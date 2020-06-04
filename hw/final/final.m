18ed713a6679c4991d2f93095f26b91c8d459093
alex hay <alexanderhay2020@u.northwestern.edu>
2020-06-04 17:01:48 -0500

    problem 2 done

--- hw/final/final.m
+++ hw/final/final.m
@@ -66,6 +66,7 @@ fprintf('\n');
 fprintf('1g\n');
 fprintf('p-values indicate the strength of the evidence against the null-hypothesis. A p-value of 0.05 shows a 5% probability\n');
 fprintf('that the null hypothesis is correct\n');
+fprintf('\n');
 
 %% Problem Set 2
 % ***********************************************
@@ -75,7 +76,7 @@ rat = linspace(1,15,15);
 lo_anx = [3,6,3,7,7,2,3,5,8,10,2,5,7,8,11];
 hi_anx = [5,6,7,11,8,4,8,9,12,17,15,3,16,14,9];
 
-fprintf('2a\n');
+fprintf('2a ********** \n');
 fprintf('Low anxiety\n');
 fprintf('mean:\t%.2f \t sigma:\t%.2f\n',mean(lo_anx),std(lo_anx));
 fprintf('\n');
@@ -102,7 +103,7 @@ lo_anx_int_hi = mean_lo_anx + (1.96 * sem_hi_anx);
 hi_anx_int_lo = mean_hi_anx - (1.96 * sem_lo_anx);
 hi_anx_int_hi = mean_hi_anx + (1.96 * sem_hi_anx);
 
-fprintf('2b\n');
+fprintf('2b ********** \n');
 fprintf('Low anxiety\n');
 fprintf('95%% interval: %.2f - %.2f\n', lo_anx_int_lo, lo_anx_int_hi);
 fprintf('\n');
@@ -113,3 +114,56 @@ fprintf('\n');
 %% **********************************************
 % 2c
 
+fprintf('2c ********** \n');
+fprintf('A two sample t-test would determine if the data comes from different populations or not.\n');
+fprintf('In this context it would determine if each condition have different means (effects).\n');
+fprintf('\n');
+
+%% **********************************************
+% 2d
+
+fprintf('2d ********** \n');
+fprintf('Null hypothesis: that both conditions have the same means\n');
+fprintf('μ_lo_anx = μ_hi_anx\n')
+fprintf('\n');
+fprintf('Alt hypothesis: that both conditions have the different means\n');
+fprintf('μ_lo_anx ≠ μ_hi_anx\n')
+fprintf('\n');
+
+%% **********************************************
+% 2e
+
+[h p ci stats] = ttest2(lo_anx, hi_anx);
+
+fprintf('2e ********** \n');
+fprintf('Two-sample t-test results:\n');
+fprintf('h:\t%.f\n',h);
+fprintf('DoF:\t%.f\n',stats.df);
+fprintf('p:\t%.4f\n',p);
+fprintf('\n');
+
+%% **********************************************
+% 2f
+
+fprintf('2f ********** \n');
+fprintf('The results of the two-sample t-test indicate that the data from each condition represent different effects\n');
+fprintf('\n');
+
+%% **********************************************
+% 2g
+
+fprintf('2g ********** \n');
+fprintf('Statistical power:\t\t%.2f\n',sampsizepwr('t2',[mean_lo_anx std_lo_anx],mean_hi_anx,[],n));
+fprintf('Rats needed for power of 0.95:\t%.f\n',sampsizepwr('t2',[mean_lo_anx std_lo_anx],mean_hi_anx,.95,[]));
+fprintf('Statistical power is the likelihood of the test rejecting the hypothesis\n');
+fprintf('A power of 0.95 demonstrates a 95%% confidence of rejecting the null hypothesis\n');
+fprintf('\n');
+
+%% **********************************************
+% 2h
+
+fprintf('2h ********** \n');
+fprintf('Using the same rats would mean using a paired t-test\n');
+fprintf('Statistical power:\t%.2f\n',sampsizepwr('t',[mean_lo_anx std_lo_anx],mean_hi_anx,[],n));
+fprintf('Advantages:\trequires fewer rats\n');
+fprintf('\t\teliminates variations between rats\n');
